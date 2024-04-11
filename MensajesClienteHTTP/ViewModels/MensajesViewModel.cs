136e5fe5843ee8b82217ed51c9745fed54927723
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2024-04-10 19:38:30 -0600

    update

--- MensajesClienteHTTP/ViewModels/MensajesViewModel.cs
+++ MensajesClienteHTTP/ViewModels/MensajesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MensajesClienteHTTP.Models;
 using MensajesClienteHTTP.Services;
 using System;
@@ -7,22 +8,52 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace MensajesClienteHTTP.ViewModels
 {
-    public class MensajesViewModel:ObservableObject
+    public partial class MensajesViewModel:ObservableObject
     {
         DiscoveryService discoveryService=new();
         MensajesService mensajesService=new MensajesService();
+        public MensajesDto mensajesDto { get; set; }=new MensajesDto();
+        public Server Seleccionado { get; set; } = null!;
+        public List<SolidColorBrush> Colores { get; set; }=new List<SolidColorBrush>();
         public ObservableCollection<Server> Servers { get; set; } =new ObservableCollection<Server>();
+
+        [RelayCommand]
+        void Enviar()
+        {
+            mensajesService.EnviarMensaje(Seleccionado, mensajesDto);
+        }
         public MensajesViewModel()
         {
+            foreach (var item in typeof(Brushes).GetProperties())
+            {
+                Colores.Add((SolidColorBrush) (item.GetValue(null)?? new SolidColorBrush()));
+            }
             discoveryService.ServidorRecibido += DiscoveryService_ServidorRecibido;
         }
 
         private void DiscoveryService_ServidorRecibido(object? sender, Models.Server e)
         {
-            Servers.Add(e);
+            var server=Servers.FirstOrDefault(x=>x.NombreServer==e.NombreServer);
+            if (server == null)
+            {
+
+                Servers.Add(e);
+            }
+            else
+            {
+                server.KeepAlive = e.KeepAlive;
+            }
+            foreach (var item in Servers.ToList())
+            {
+                if((DateTime.Now- item.KeepAlive).TotalSeconds > 30)
+                {
+                    Servers.Remove(item);
+                }
+            }
         }
     }
 }
